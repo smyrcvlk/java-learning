cdb23fc3189fbf555073ba634f497a2a5f42ab4f
smyrcvlk <smyrcvlk@gmail.com>
2019-10-27 22:40:38 -0400

    arrays

---
+++ test2/src/OCT_27_19/day15homework.java
@@ -0,0 +1,49 @@
+package OCT_27_19;
+
+public class day15homework {
+    public static void main(String[] args) {
+
+// 1. Return the number of even ints in the given array.
+        // Hint: you need to count "even numbers"
+        // 2.Given an array of ints, print "has 1 and 2" if there is a 1 in the array
+        //    with a 2 somewhere later in the array.
+        // Hint: if you array has 1 and after that somewhere has 2, is should print "has 1 and 2"
+        //
+
+        int[] myArray = {1, 2, 3, 4, 5, 6, 1111};
+        // part 1
+        System.out.println("Part 1 : ");
+        System.out.println("=====================");
+        for (int i = 0; i < myArray.length; i++) {
+
+            if (myArray[i] % 2 == 0) {
+                System.out.println("Even numbers are : " + myArray[i]);
+            }
+        }
+
+        // part 2
+        System.out.println("");
+        System.out.println("Part 2 : ");
+        System.out.println("=====================");
+        boolean condition1 = false;
+        boolean condition2 = false;
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == 1 ){
+                condition1=true;
+            }
+            if(myArray[i]==2){
+                condition2=true;
+
+            }
+        }
+        if (condition1==true && condition2==true) {
+            System.out.println("my array has 1 and 2" );
+        } else {
+            System.out.println("there aren't 1 and 2 ");
+        }
+    }
+}
+
+
+
